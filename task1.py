852198e6a0500e30c985c0cf27243353ab5fc17f
Elena Kantonistova <elena.kantonistova@yandex.ru>
2018-11-10 22:49:26 +0300

    '4'

--- task1.py
+++ task1.py
@@ -15,13 +15,14 @@ def main_page():
             res = 'sum:'+str(a + b)
         elif 'raznost' in request.form:
             res = 'razn:'+str(a - b) 
-        elif 'radio_sum' in request.form:
-            if 'radio_razn' in request.form:
-                res = 'sum:'+str(a+b)+', razn:'+str(a-b)
-            else:
-                res = 'sum:'+str(a + b)
-        elif 'radio_razn' in request.form:
-            res = 'razn:'+str(a - b)
+        if 'Submit Answer' in request.form:
+            elif 'radio_sum' in request.form:
+                if 'radio_razn' in request.form:
+                    res = 'sum:'+str(a+b)+', razn:'+str(a-b)
+                else:
+                    res = 'sum:'+str(a + b)
+            elif 'radio_razn' in request.form:
+                res = 'razn:'+str(a - b)
         
     return render_template('main_page.html',params={'a':a,'b':b,'res':res})
 
